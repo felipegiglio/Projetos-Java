3ebda390f8227f3f93a5d4d3f119845f1527dd70
Felipe Giglio <felipe.gigliosilva@gmail.com>
2022-12-02 09:40:36 -0300

    Primeiro Programa

---
+++ primeiro.java
@@ -0,0 +1,10 @@
+public class primeiro{
+    public static void main(String[]args) {
+        double a;
+        int b;
+
+        a = 5.0;
+        b = (int)a;
+        System.out.println(b);
+    }        
+}
