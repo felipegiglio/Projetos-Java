762cb649a4b628ecf94c97eda677193716b72209
Felipe Giglio <felipe.gigliosilva@gmail.com>
2022-12-02 15:42:55 -0300

    Novos

--- ex1.java
+++ ex1.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
+
 public class ex1 {
-    public static void main(String[]args) {
-      Scanner ler = new Scanner(System.in);
-      System.out.println(ler);
+    public static void main(String[] args) {
+        Double valor, cotacao;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o valor em Dolar");
+        valor = entrada.nextDouble();
+        cotacao = (valor * 5.21);
+        System.out.println("O valor digitado é: "+cotacao);
+
+
     }
 }
---
+++ saidadedados.java
@@ -0,0 +1,9 @@
+import java.util.Locale;
+
+public class saidadedados {
+    public static void main(String[]args) {
+        double x = 10.5;
+        System.out.println(x);
+        Locale.setDefault(Locale.US);
+    }
+}
